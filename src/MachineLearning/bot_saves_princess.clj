1643382bdb8bbd2847534d54a0d523b9993d52c7
Ray Pendergraph <ray.pendergraph@janusresearch.com>
2015-06-29 15:53:57 -0400

    Machine Learning stuff.

--- src/MachineLearning/bot_saves_princess.clj
+++ src/MachineLearning/bot_saves_princess.clj
@@ -1,49 +1,7 @@
 (ns bot-saves-princess)
 
-(defn point [x y]
-  (double-array [x y]))
-
-(defn discreet-point [[real-x real-y]]
-  [(int real-x) (int real-y)])
-
-(defn vector-magnitude [vector]
-  (let [square (fn [x] (* x x))]
-    (Math/sqrt
-      (reduce + (for [x vector] (square x))))))
-
-(defn vector-norm [vector]
-  (let [length (vector-magnitude vector)]
-    (for [i vector]
-      (/ i length))))
-
-(defn vector-subtract [source destination]
-  (map - destination source))
-
-(defn vector-add [source destination]
-  (map + destination source))
-
-(defn vector-scalar-multiply [vector scalar]
-  (map (fn [x] (* scalar x)) vector))
-
-(defn square [x] (* x x))
-
-(defn segment-length [[x-source y-source] [x-dest y-dest]]
-   (Math/sqrt (+ (square (- x-dest x-source))
-                 (square (- y-dest y-source)))))
-
-(defn calculate-steps [start dest]
-  (int (Math/ceil (segment-length start dest))))
-
-(defn next-step [start direction step]
-  (discreet-point (vector-add start
-                              (vector-scalar-multiply direction step))))
-
 (def dest (point 1 1))
 (def start (point 4 9))
-;(let [step-count (calculate-steps start dest)
-;      direction  (vector-norm (vector-subtract start dest))]
-;  (doseq [step (for [n (range 1 step-count)] (next-step start direction n))]
-;    (println step )))
 
 (defn create-step-function [source destination]
     (cond
