212db49a2913df296759e0aa8f03998afb6fe8ac
ray.pendergraph@gmx.com <ray.pendergraph@gmx.com>
2015-08-10 19:54:20 -0400

    Initial

--- src/MachineLearning/bot_saves_princess.clj
+++ src/MachineLearning/bot_saves_princess.clj
@@ -1,32 +1,56 @@
 (ns bot-saves-princess)
 
-(def dest (point 1 1))
-(def start (point 4 9))
+(def grid [[\- \- \-] [\- \m \-] [\p \- \-]])
 
 (defn create-step-function [source destination]
-    (cond
-      (> source destination) (fn [current]
-                               (if (> current destination) (dec current) current))
-      (< source destination) (fn [current]
-                               (if (< current destination) (inc current) current))
-      :else (fn [current] current)))
-(defn moves-remaining? [x y dest-x dest-y]
+  (cond
+    (> source destination) (fn [current]
+                             (if (> current destination) (dec current) current))
+    (< source destination) (fn [current]
+                             (if (< current destination) (inc current) current))
+    :else (fn [current] current)))
+
+(defn moves-remaining? [row column dest-row dest-column]
   (or
-    (not (= x dest-x))
-    (not (= y dest-y))))
+    (not (= column dest-column))
+    (not (= row dest-row))))
 
-(defn print-step [old-x old-y new-x new-y]
-  (println old-x old-y " => " new-x new-y))
+(defn interleave-all
+  [& colls]
+  (lazy-seq
+    (let [ss (keep seq colls)]
+      (when-not (empty? ss)
+        (concat (map first ss)
+                (apply super-fn (map rest ss)))))))
 
-(let [[source-x source-y] start
-      [dest-x dest-y]     dest
-      step-horizontal  (create-step-function source-x dest-x)
-      step-vertical    (create-step-function source-y dest-y)]
-  (loop [x source-x
-         y source-y]
-    (when (moves-remaining? x y dest-x dest-y)
-        (let [new-x (step-horizontal x)
-              new-y (step-vertical y)]
+(defn generate-steps [start-row start-column end-row end-column]
+  )
+
+(defn coordinates [row column]
+  [row column])
+
+(defn map-row-results [row-results row-number]
+  (reduce
+    (fn [m [column-number character]]
+      (assoc m character (coordinates row-number column-number))) {} row-results))
+
+(defn search-row [row]
+  (filter
+    (fn [[i c]] (not= c \-))
+    (map-indexed vector row)))
+
+(defn find-steps [grid]
+  (let [{[m-row m-column] \m [p-row p-column] \p} (into {} (for [row (map-indexed vector grid)]
+                                                             (map-row-results (search-row (nth row 1)) (nth row 0))))
+        row-step-fn (create-step-function m-row p-row)
+        column-step-fn (create-step-function m-column p-column)]
+    (loop [row m-row
+           column m-column]
+      (when (moves-remaining? row column p-row p-column)
+        (let [new-row (row-step-fn row)
+              new-column (column-step-fn column)]
           (do
-            (print-step x y new-x new-y)
-            (recur  new-x new-y))))))
+            (print-step row column new-row new-column)
+            (recur new-row new-column)))))))
+
+(find-steps grid)
